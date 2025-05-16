e2d80b65b6498e0ed14a4150cd9606800e6cdfbf
MD. Shohel Rana <shohel065931@gmail.com>
2025-05-16 20:30:38 +0600

    tt

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php 
+
+// Css and JS Calling
+function travel_enqueue_scripts(){
+    wp_enqueue_style(' style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri(). '/css/bootstrap.css', array(), '5.0.2', 'all');
+    wp_register_style('main', get_template_directory_uri(). '/css/main.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('main');
+
+
+
+    // JS
+    wp_enqueue_script( 'jquary');
+    wp_enqueue_script('bootstrap', get_template_directory_uri(). '/js/bootstrap.js', array(), '5.0.2', true);
+    wp_enqueue_script('main', get_template_directory_uri(). '/js/main.js', array(), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'travel_enqueue_scripts');
