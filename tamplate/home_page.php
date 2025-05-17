f4640a934dc7bc3c23ad5ec21b5d8b2e46da8ad0
MD. Shohel Rana <shohel065931@gmail.com>
2025-05-17 18:53:22 +0600

    tt

--- tamplate/home_page.php
+++ tamplate/home_page.php
@@ -6,7 +6,20 @@
 get_header();
 ?>
 
-
+<section id="home_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="home_area_content">
+                    <h1>Welcome to Our Website</h1>
+                    <p>Your one-stop solution for all your needs.</p>
+                    <a href="#services" class="btn btn-primary">Explore Services</a>
+                    <a href="#services" class="btn btn-primary">Explore Services</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 get_footer();
